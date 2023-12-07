6cb18bcd05851217bd8f8e17b0e20a9b3d45cbeb
Alex Baltazar <alexbaltazarcastellon141@gmail.com>
2023-12-06 23:06:24 -0400

    Doble de un numero

---
+++ doble_entero.py
@@ -0,0 +1,13 @@
+"""
+Duplicar el número entero y devolverlo.
+"""
+
+
+def doble_entero(i):
+    """Funcion para sacar el doble de un numero"""
+    return pow(i,2)
+
+
+a = 12
+
+print(doble_entero(a))
